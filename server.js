721b4b71c778ea89219fcf41786494f577577a5a
Shital Mane <shitalma@thoguthworks.com>
2018-10-07 09:17:28 +0530

    Give support to run server side custom api

--- server.js
+++ server.js
@@ -2,19 +2,25 @@ const express = require('express'),
     next = require('next');
 
 const dev = process.env.NODE_ENV !== 'production',
-    app = next({dev}),
+    app = next({ dev }),
     handle = app.getRequestHandler();
 
 app.prepare()
     .then(() => {
         const server = express();
 
+        server.get('/p/:id', (req, res) => {
+            const actualPage = '/post',
+                queryParams = { title: req.params.id };
+            app.render(req, res, actualPage, queryParams)
+        });
+
         server.get('*', (req, res) => {
             return handle(req, res)
         });
 
         server.listen(3000, (err) => {
-            if (err) throw err;
+            if (err) throw err
             console.log('> Ready on http://localhost:3000')
         })
     })
